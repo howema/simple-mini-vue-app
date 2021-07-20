896c3e3b82ca8c3c2825d75e93279cb763494ea8
MEG HOWE <howem9@gmail.com>
2021-07-20 16:25:26 -0400

    Not an initial commit by any means

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -5,29 +5,49 @@
     <p>{{ message2 }}</p>
     <h1>New Product</h1>
     <div>
+      <p></p>
       Name:
       <input type="text" v-model="newProductParams.name" />
+      <p></p>
       Description:
       <input type="text" v-model="newProductParams.description" />
+      <p></p>
       Price:
       <input type="text" v-model="newProductParams.price" />
+      <p></p>
       Image_url:
       <input type="text" v-model="newProductParams.image_url" />
+      <p></p>
     </div>
     <button v-on:click="createProduct()">Create</button>
     <div v-for="product in products" :key="product.id">
       <h1>{{ product.name }}</h1>
       <h2>{{ product.price }}</h2>
       <img v-bind:src="product.image_url" alt="product.name" />
+      <p></p>
       <button v-on:click="showProduct(product)">More info!</button>
+      <button v-on:click="updateProduct(product)">Update!</button>
+      <button v-on:click="destroyProduct(product)">Destroy!</button>
     </div>
     <dialog id="product-details">
       <form method="dialog">
         <h1>Product Info!</h1>
-        <p>Name: ...</p>
-        <p>Description: ...</p>
-        <p>Price: ...</p>
-        <p>Image_url: ...</p>
+        <p>
+          Name:
+          <input type="text" v-model="currentProduct.name" />
+        </p>
+        <p>
+          Description:
+          <input type="text" v-model="currentProduct.description" />
+        </p>
+        <p>
+          Price:
+          <input type="text" v-model="currentProduct.price" />
+        </p>
+        <p>
+          Image_url:
+          <input type="text" v-model="currentProduct.image_url" />
+        </p>
         <button>Close</button>
       </form>
     </dialog>
@@ -44,6 +64,7 @@ export default {
       message2: "Teeny owl feet",
       products: [],
       newProductParams: {},
+      currentProduct: {},
     };
   },
   created: function () {
@@ -68,7 +89,21 @@ export default {
     },
     showProduct: function (product) {
       console.log(product);
+      this.currentProduct = product;
       document.querySelector("#product-details").showModal();
+    },
+    updateProduct: function (product) {
+      var editProductParams = product;
+      axios.patch("http://localhost:3000/products/" + product.id, editProductParams).then((response) => {
+        console.log("You did it!", response.data);
+      });
+    },
+    destroyProduct: function (product) {
+      axios.delete("http://localhost:3000/products/" + product.id).then((response) => {
+        console.log("Success!", response.data);
+        var index = this.products.indexOf(product);
+        this.products.splice(index, 1);
+      });
     },
   },
 };
